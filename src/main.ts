4c22f92eb3402932cba0059fc499d54592a7f66a
Bernardo Amaral <dev02@rksoftware.com.br>
2024-10-01 15:55:10 -0300

    Add RabbitMq

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const rabbitMqConfig = {
+    transport: WebTransport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'cats_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  };
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      logger: ['error', 'warn'],
+      ...rabbitMqConfig,
+    },
+  );
   await app.listen(3000);
 }
 bootstrap();
